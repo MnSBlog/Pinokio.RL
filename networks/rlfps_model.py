8ac00f8182ce9d293bf8f6de618be3a849a5e148
Simon Anderson <bbubgsang94@ajou.ac.kr>
2022-08-17 12:16:40 +0900

    tt

--- networks/rlfps_model.py
+++ networks/rlfps_model.py
@@ -5,13 +5,15 @@ import torch.nn as nn
 class RLModel(nn.Module):
     def __init__(self, args):
         super().__init__()
+
+        self.model_list = []
         # Actor & Critic
         for _ in range(args['n_of_actor_networks']):
             test = 1
 
         for _ in range(args['n_of_critic_networks']):
             test = 2
-            
+
         # Optimizer
         if args['optimizer'] == 'adam'
             self.optimizer = torch.optim.adam()
