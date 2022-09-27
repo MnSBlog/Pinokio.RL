061182ea7b8654504cc084ac8b373c643b2ae260
BYEONGSEONLEE <tjs5007@ajou.ac.kr>
2022-09-27 15:02:49 +0900

    BS0927

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,5 @@
 from envs.rlfps.rlfps_v4 import RLFPSv4
+from envs.dogfight.dogfight_v1 import Dogfightv1
 from functools import partial
 import gym
 
@@ -7,4 +8,5 @@ def get_env_fn(env, **kwargs) -> gym.Env:
     return env(kwargs)
 
 
-REGISTRY = {'RLFPSv4': partial(get_env_fn, env=RLFPSv4)}
+REGISTRY = {'RLFPSv4': partial(get_env_fn, env=RLFPSv4),
+            'Dogfightv1': partial(get_env_fn, env=Dogfightv1)}
