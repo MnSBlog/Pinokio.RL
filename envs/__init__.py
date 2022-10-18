ad16dbf97d95dc6b31ef1ff624ebcb3a07a96f5b
woose98@ajou.ac.kr <woose98@ajou.ac.kr>
2022-10-18 23:21:50 +0900

    PathFindSim Added.
    Signed-off-by: woose98@ajou.ac.kr <woose98@ajou.ac.kr>

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,9 +1,6 @@
 from envs.rlfps.rlfps_v4 import RLFPSv4
-<<<<<<< HEAD
-from envs.dogfight.dogfight_v1 import Dogfightv1
-=======
 from envs.ohtsim.oht_routing import OhtBase
->>>>>>> 8bb0add5a694264079f7cdbe3732d9a7bb4b4514
+from envs.pathfindSim.pathfind_v1 import PathFindSim
 from functools import partial
 import gym
 
@@ -13,8 +10,5 @@ def get_env_fn(env, **kwargs) -> gym.Env:
 
 
 REGISTRY = {'RLFPSv4': partial(get_env_fn, env=RLFPSv4),
-<<<<<<< HEAD
-            'Dogfightv1': partial(get_env_fn, env=Dogfightv1)}
-=======
-            'OHTRouting': partial(get_env_fn, env=OhtBase)}
->>>>>>> 8bb0add5a694264079f7cdbe3732d9a7bb4b4514
+            'OHTRouting': partial(get_env_fn, env=OhtBase),
+            'PathFindSim': partial(get_env_fn, env=PathFindSim)}
